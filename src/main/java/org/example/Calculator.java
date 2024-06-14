f25d55acdd8223bcaa039b40ac43cf1f01dd2c8c
trtien05 <trantien100700@gmail.com>
2024-06-14 23:09:38 +0700

    NNS-15 test_email

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,7 +3,7 @@ package org.example;
 public class Calculator {
 
     public int add(int a, int b) {
-        return a + b;
+        return a +  b;
     }
 
     public int substract(int a, int b) {
